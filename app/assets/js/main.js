da0a48a2b377cf357e91426bffd901649274ad53
vasilie <vasilie.91@gmail.com>
2017-04-24 23:30:37 +0200

    handlebars templating, svg map

---
+++ app/assets/js/main.js
@@ -0,0 +1,17 @@
+var paths = [2,18,23,37,42,43,45];
+var country1 = $(g3248[2][0]);
+var country2 = $(g3248[18][0]);
+var country3 = $(g3248[23][0]);
+var country4 = $(g3248[37][0]);
+var country5 = $(g3248[42][0]);
+var country6 = $(g3248[43][0]);
+var country7 = $(g3248[45][0]);
+var country7 = $(g3248[45][0]);
+
+country1.css({"fill":"red"},100);
+country2.css({"fill":"blue"},100);
+country3.css({"fill":"green"},100);
+country4.css({"fill":"pink"},100);
+country5.css({"fill":"yellow"},100);
+country6.css({"fill":"orange"},100);
+country6.css({"fill":"purple"},100);
